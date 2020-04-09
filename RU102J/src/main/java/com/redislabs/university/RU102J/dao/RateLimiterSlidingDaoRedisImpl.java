c6289c65a546924231b5ff6a6bb8a12ef454e575
Hayden <meloche.hayden@gmail.com>
2020-04-09 19:13:30 -0400

    Initial commit

---
+++ RU102J/src/main/java/com/redislabs/university/RU102J/dao/RateLimiterSlidingDaoRedisImpl.java
@@ -0,0 +1,30 @@
+package com.redislabs.university.RU102J.dao;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
+
+import java.time.ZonedDateTime;
+import com.redislabs.university.RU102J.core.KeyHelper;
+
+public class RateLimiterSlidingDaoRedisImpl implements RateLimiter {
+
+    private final JedisPool jedisPool;
+    private final long windowSizeMS;
+    private final long maxHits;
+
+    public RateLimiterSlidingDaoRedisImpl(JedisPool pool, long windowSizeMS,
+                                          long maxHits) {
+        this.jedisPool = pool;
+        this.windowSizeMS = windowSizeMS;
+        this.maxHits = maxHits;
+    }
+
+    // Challenge #7
+    @Override
+    public void hit(String name) throws RateLimitExceededException {
+        // START CHALLENGE #7
+        // END CHALLENGE #7
+    }
+}
